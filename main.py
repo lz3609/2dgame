e9affb41ef55c7d0727d1f5677cd8d9fe1ba5c8b
Lin Zheng <linzheng3609@gmail.com>
2024-09-22 19:07:21 -0400

    c

---
+++ main.py
@@ -0,0 +1,21 @@
+import pygame
+import sys
+
+pygame.init()
+
+screen_width = 1600
+screen_height = 900
+
+pygame.display.set_caption("game")
+screen = pygame.display.set_mode((screen_width, screen_height))
+
+clock = pygame.time.Clock()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        
+    pygame.display.update()
+    clock.tick(60)

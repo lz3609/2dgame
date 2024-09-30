b2d38cdaa87059966aa0ab3ab4423e6268a8d457
Lin Zheng <linzheng3609@gmail.com>
2024-09-29 23:46:55 -0400

    Added class

--- main.py
+++ main.py
@@ -1,21 +1,28 @@
 import pygame
 import sys
 
-pygame.init()
-
 screen_width = 1600
 screen_height = 900
 
-pygame.display.set_caption("game")
-screen = pygame.display.set_mode((screen_width, screen_height))
+class Game:
+    def __init__(self):
+        pygame.init()
 
-clock = pygame.time.Clock()
-
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
+        pygame.display.set_caption("game")
+        self.screen = pygame.display.set_mode((screen_width, screen_height))
+        self.clock = pygame.time.Clock()
+        self.background = pygame.image.load('images/background.png')
+        self.background = pygame.transform.scale(self.background, (screen_width, screen_height))
+    
+    def run(self):
+        while True:
+            self.screen.blit(self.background, (0,0))
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
         
-    pygame.display.update()
-    clock.tick(60)
+            pygame.display.update()
+            self.clock.tick(60)
+
+Game().run()
